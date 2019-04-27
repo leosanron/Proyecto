8abd3374d95a80d45c32d7c965aa1ebc90c84755
Leonardo <leosanron@outlook.com>
2019-04-27 14:39:42 +0000

    chapter three finished

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  
+
   def setup
-    @base_title = "Exam one"
+    @base_title = "Ruby on Rails Tutorial Sample App"
   end
   
   test "should get basics" do
