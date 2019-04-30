df0cf6539cd8b1c339925fe454da4e041b595f53
Leonardo <leosanron@outlook.com>
2019-04-30 00:27:05 +0000

    not complete

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,15 +1,18 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-  test "layout links" do
-    get central_path
+  
+   test "layout links" do
+     
+    get root_path
     assert_template 'static_pages/central'
-    assert_select "a[href=?]", basics_path, count: 2
+    assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", git_path
+    assert_select "a[href=?]", basics_path
     assert_select "a[href=?]", ruby_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", central_path
   end
+  
 end
